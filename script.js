dbabd492aa4a9daa1653c7a4da8d149ba6776008
AGpeter2018 <adenijipeter2018@gmail.com>
2025-08-02 09:34:50 +0100

    functionalitie was added to the projects whereby the info box and are show by button and a continue buttton to the quiz box

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
- 
- 
+const startBtn = document.querySelector(".start-btn");
+const infoBox = document.querySelector(".info-box");
+const quizBox = document.querySelector(".quiz-box");
+const quitBtn = document.querySelector(".quit");
+const continueBtn = document.querySelector(".restart");
+
+startBtn.addEventListener("click", () => {
+  startBtn.style.opacity = "0";
+  infoBox.classList.add("info-box-active-info");
+});
+
+quitBtn.addEventListener("click", () => {
+  infoBox.classList.remove("info-box-active-info");
+  startBtn.style.opacity = "1";
+});
+
+continueBtn.addEventListener("click", () => {
+  infoBox.classList.remove("info-box-active-info");
+  quizBox.classList.add("quiz-box-active-box");
+});
